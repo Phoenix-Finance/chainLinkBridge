60008732eacc85416932dc67cb134cbf18cfc518
SmithAmbition <cranelvjq@163.com>
2020-10-29 17:40:18 +0800

    add new functions

--- command/rinkeby.js
+++ command/rinkeby.js
@@ -6,16 +6,22 @@ let FNXOracle = require("../build/contracts/FNXOracle.json");
 let ImpliedVolatility = require("../build/contracts/ImpliedVolatility.json");
 let daySeconds = 24*3600;
 async function rinkebyQuery(){
-    let oracle = await new web3.eth.Contract(FNXOracle.abi,"0x950812ac76fACF39236dBD87dBe388bc96a3B311");
+    let oracle = await new web3.eth.Contract(FNXOracle.abi,"0xAFd44Ac5Ac276869B24Fdbc604f1fBbB67F37009");
     let price = await oracle.methods.getUnderlyingPrice(1).call();
     console.log("BTC Price :",price.toString(10));
     price = await oracle.methods.getUnderlyingPrice(2).call();
     console.log("ETH Price :",price.toString(10));
-    price = await oracle.methods.getPrice("0xc69a38684D6Db1beaB5029093B1d19CE7595f130").call();
+    price = await oracle.methods.getUnderlyingPrice(3).call();
+    console.log("MKR Price :",price.toString(10));
+    price = await oracle.methods.getUnderlyingPrice(4).call();
+    console.log("SNX Price :",price.toString(10));
+    price = await oracle.methods.getUnderlyingPrice(5).call();
+    console.log("LINK Price :",price.toString(10));
+    price = await oracle.methods.getPrice("0xD12BC93Ac5eA2b4Ba99e0ffEd053a53B6d18C7a3").call();
     console.log("USDC Price :",price.toString(10));
     price = await oracle.methods.getPrice(collateral0).call();
     console.log("ETH Price :",price.toString(10));
-    price = await oracle.methods.getPrice("0xB642Cef1cffD9cCEa0e6724887b722B27A3E7D23").call();
+    price = await oracle.methods.getPrice("0xaf30F6A6B09728a4e793ED6d9D0A7CcBa192c229").call();
     console.log("FNX Price :",price.toString(10));
 
 }
--- contracts/FNXOracle.sol
+++ contracts/FNXOracle.sol
@@ -4,8 +4,8 @@ import "./interfaces/AggregatorV3Interface.sol";
 import "./interfaces/IERC20.sol";
 
 contract FNXOracle is Operator {
-    mapping(uint256 => AggregatorV3Interface) underlyingsMap;
-    mapping(address => AggregatorV3Interface) assetsMap;
+    mapping(uint256 => AggregatorV3Interface) private assetsMap;
+    mapping(uint256 => uint256) private decimalsMap;
     mapping(uint256 => uint256) private priceMap;
     uint256 internal decimals = 1;
 
@@ -15,13 +15,57 @@ contract FNXOracle is Operator {
      * Address: 0x727B59d0989d6D1961138122BC9F94f534E82B32
      */
     constructor() public {
-        underlyingsMap[1] = AggregatorV3Interface(0xECe365B379E1dD183B20fc5f022230C044d51404);
-        underlyingsMap[2] = AggregatorV3Interface(0x8A753747A1Fa494EC906cE90E9f37563A8AF630e);
-        assetsMap[0x0000000000000000000000000000000000000000] = AggregatorV3Interface(0x8A753747A1Fa494EC906cE90E9f37563A8AF630e);
-//        assetsMap[0x4738635C82BED8F474D9A078F4E5797fa5d5f460] = AggregatorV3Interface(0xa24de01df22b63d23Ebc1882a5E3d4ec0d907bFB);
+        /*
+        mainnet
+        assetsMap[1] = AggregatorV3Interface(0xF4030086522a5bEEa4988F8cA5B36dbC97BeE88c);
+        assetsMap[2] = AggregatorV3Interface(0x5f4eC3Df9cbd43714FE2740f5E3616155c5b8419);
+        assetsMap[3] = AggregatorV3Interface(0x5f4eC3Df9cbd43714FE2740f5E3616155c5b8419);
+        assetsMap[4] = AggregatorV3Interface(0xDC3EA94CD0AC27d9A86C180091e7f78C683d3699);
+        assetsMap[5] = AggregatorV3Interface(0x2c1d072e956AFFC0D435Cb7AC38EF18d24d9127c);
+        assetsMap[0] = AggregatorV3Interface(0x5f4eC3Df9cbd43714FE2740f5E3616155c5b8419);
+        assetsMap[uint256(0xeF9Cd7882c067686691B6fF49e650b43AFBBCC6B)] = AggregatorV3Interface(0x80070f7151BdDbbB1361937ad4839317af99AE6c);
+        priceMap[uint256(0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48)] = 1e20;
+        decimalsMap[0] = 18;
+        decimalsMap[1] = 18;
+        decimalsMap[2] = 18;
+        decimalsMap[3] = 18;
+        decimalsMap[4] = 18;
+        decimalsMap[5] = 18;
+        decimalsMap[uint256(0xeF9Cd7882c067686691B6fF49e650b43AFBBCC6B)] = 18;
+        decimalsMap[uint256(0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48)] = 6;
+        */
+        //rinkeby
+        assetsMap[1] = AggregatorV3Interface(0xECe365B379E1dD183B20fc5f022230C044d51404);
+        assetsMap[2] = AggregatorV3Interface(0x8A753747A1Fa494EC906cE90E9f37563A8AF630e);
+        assetsMap[3] = AggregatorV3Interface(0xd8bD0a1cB028a31AA859A21A3758685a95dE4623);
+        assetsMap[4] = AggregatorV3Interface(0xE96C4407597CD507002dF88ff6E0008AB41266Ee);
+        assetsMap[5] = AggregatorV3Interface(0xd8bD0a1cB028a31AA859A21A3758685a95dE4623);
+        assetsMap[0] = AggregatorV3Interface(0x8A753747A1Fa494EC906cE90E9f37563A8AF630e);
+        assetsMap[uint256(0xaf30F6A6B09728a4e793ED6d9D0A7CcBa192c229)] = AggregatorV3Interface(0xcf74110A02b1D391B27cE37364ABc3b279B1d9D1);
+        priceMap[uint256(0xD12BC93Ac5eA2b4Ba99e0ffEd053a53B6d18C7a3)] = 1e20;
+        decimalsMap[0] = 18;
+        decimalsMap[1] = 18;
+        decimalsMap[2] = 18;
+        decimalsMap[3] = 18;
+        decimalsMap[4] = 18;
+        decimalsMap[5] = 18;
+        decimalsMap[uint256(0xaf30F6A6B09728a4e793ED6d9D0A7CcBa192c229)] = 18;
+        decimalsMap[uint256(0xD12BC93Ac5eA2b4Ba99e0ffEd053a53B6d18C7a3)] = 6;
+
     }
     function setDecimals(uint256 newDecimals) public onlyOwner{
         decimals = newDecimals;
+    }
+    function getAssetAndUnderlyingPrice(address asset,uint256 underlying) public view returns (uint256,uint256) {
+        return (getUnderlyingPrice(uint256(asset)),getUnderlyingPrice(underlying));
+    }
+    function getPrices(uint256[]memory assets) public view returns (uint256[]memory) {
+        uint256 len = assets.length;
+        uint256[] memory prices = new uint256[](len);
+        for (uint i=0;i<len;i++){
+            prices[i] = getUnderlyingPrice(assets[i]);
+        }
+        return prices;
     }
         /**
   * @notice retrieves price of an asset
@@ -30,30 +74,20 @@ contract FNXOracle is Operator {
   * @return uint mantissa of asset price (scaled by 1e8) or zero if unset or contract paused
   */
     function getPrice(address asset) public view returns (uint256) {
-        AggregatorV3Interface assetsPrice = assetsMap[asset];
-        if (address(assetsPrice) != address(0)){
-            (, int price,,,) = assetsPrice.latestRoundData();
-            if (asset != address(0)){
-                IERC20 token = IERC20(asset);
-                uint256 tokenDecimals = uint256(token.decimals());
-                if (tokenDecimals < 18){
-                    return uint256(price)/decimals*(10**(18-tokenDecimals));  
-                }else if (tokenDecimals > 18){
-                    return uint256(price)/decimals/(10**(18-tokenDecimals)); 
-                }else{
-                    return uint256(price)/decimals;
-                }
-            }
-            return uint256(price)/decimals; 
-        }else {
-            return priceMap[uint256(asset)];
-        }
+        return getUnderlyingPrice(uint256(asset));
     }
     function getUnderlyingPrice(uint256 underlying) public view returns (uint256) {
-        AggregatorV3Interface assetsPrice = underlyingsMap[underlying];
+        AggregatorV3Interface assetsPrice = assetsMap[underlying];
         if (address(assetsPrice) != address(0)){
             (, int price,,,) = assetsPrice.latestRoundData();
-            return uint256(price)/decimals; 
+            uint256 tokenDecimals = decimalsMap[underlying];
+            if (tokenDecimals < 18){
+                return uint256(price)/decimals*(10**(18-tokenDecimals));  
+            }else if (tokenDecimals > 18){
+                return uint256(price)/decimals/(10**(18-tokenDecimals)); 
+            }else{
+                return uint256(price)/decimals;
+            }
         }else {
             return priceMap[underlying];
         }
@@ -84,8 +118,9 @@ contract FNXOracle is Operator {
       * @param asset Asset for which to set the price
       * @param aggergator the Asset's aggergator
       */    
-    function setAssetsAggregator(address asset,address aggergator) public onlyOwner {
-        assetsMap[asset] = AggregatorV3Interface(aggergator);
+    function setAssetsAggregator(address asset,address aggergator,uint256 _decimals) public onlyOwner {
+        assetsMap[uint256(asset)] = AggregatorV3Interface(aggergator);
+        decimalsMap[uint256(asset)] = _decimals;
     }
     /**
       * @notice set price of an underlying
@@ -93,14 +128,15 @@ contract FNXOracle is Operator {
       * @param underlying underlying for which to set the price
       * @param aggergator the underlying's aggergator
       */  
-    function setUnderlyingAggregator(uint256 underlying,address aggergator) public onlyOwner {
+    function setUnderlyingAggregator(uint256 underlying,address aggergator,uint256 _decimals) public onlyOwner {
         require(underlying>0 , "underlying cannot be zero");
-        underlyingsMap[underlying] = AggregatorV3Interface(aggergator);
+        assetsMap[underlying] = AggregatorV3Interface(aggergator);
+        decimalsMap[underlying] = _decimals;
     }
-    function getAssetsAggregator(address asset) public view returns (address) {
-        return address(assetsMap[asset]);
+    function getAssetsAggregator(address asset) public view returns (address,uint256) {
+        return (address(assetsMap[uint256(asset)]),decimalsMap[uint256(asset)]);
     }
-    function getUnderlyingAggregator(uint256 underlying) public view returns (address) {
-        return address(underlyingsMap[underlying]);
+    function getUnderlyingAggregator(uint256 underlying) public view returns (address,uint256) {
+        return (address(assetsMap[underlying]),decimalsMap[underlying]);
     }
 }
